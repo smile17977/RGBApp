5b1fe75b0ae8fc1c73cdf918f820e5292fc762a4
smile17977 <65645958+smile17977@users.noreply.github.com>
2020-05-25 14:59:50 +0300

    Final version

--- RGBApp/ViewController.swift
+++ RGBApp/ViewController.swift
@@ -9,12 +9,95 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rgbView.layer.cornerRadius = 10
+        
+        // Red slider
+        redSlider.value = 0
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        redValueLabel.text = String(
+            format:"%.2f",
+            redSlider.value
+        )
+        
+        // Green slider
+        greenSlider.value = 0
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        greenValueLabel.text = String(
+            format:"%.2f",
+            greenSlider.value
+        )
+        
+        // Blue slider
+        blueSlider.value = 0
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        blueValueLabel.text = String(
+            format:"%.2f",
+            blueSlider.value
+        )
+
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        
+        redValueLabel.text = String(format:"%.2f", redSlider.value)
+        
+        let _ = CGFloat(redSlider.value)
+        
+        rgbView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    @IBAction func greenSliderAction() {
+        
+        greenValueLabel.text = String(format:"%.2f", greenSlider.value)
+        
+        let _ = CGFloat(greenSlider.value)
+        
+        rgbView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    @IBAction func blueSliderAction() {
+        
+        blueValueLabel.text = String(format:"%.2f", blueSlider.value)
+        
+        let _ = CGFloat(blueSlider.value)
+        
+        rgbView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
 }
 
